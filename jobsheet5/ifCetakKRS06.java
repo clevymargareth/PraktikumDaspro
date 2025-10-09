678485799a094a2854f336b1917d004006accf0a
clevymargareth <clevybria236@gmail.com>
2025-10-09 12:18:10 +0700

    Attempt 1

---
+++ jobsheet5/ifCetakKRS06.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS06 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--Print KRS SIAKAD--");
+        System.out.println("How the tuition fees been paid in full? (true/false): ");
+        boolean uktlunas = sc.nextBoolean();
+        if (uktlunas) {
+            System.out.println("UKT payment has been verified");
+            System.out.println("KRS now can be printed and you ask the academic advisor to sign it.");
+
+        }
+    }
+}
